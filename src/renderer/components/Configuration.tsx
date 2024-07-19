f0a7ecd55cf8dc5a7e8e645834f5cc533d580305
mireia22 <mireiagarciaferrer22@gmail.com>
2024-07-19 06:20:07 +0200

    feat: try dark mode

--- src/renderer/components/Configuration.tsx
+++ src/renderer/components/Configuration.tsx
@@ -24,15 +24,15 @@ export default function Configuration() {
       <div className="grid grid-cols-2 gap-2 p-2 ">
         <div className="flex flex-row gap-3 justify-start items-center">
           <button
-            className=" gap-x-1.5 font-bold text-xl "
+            className=" gap-x-1.5 font-bold text-xl text-black"
             id="menu-button"
             onClick={toggleDropdown}
           >
             Config
+            <svg className="-mr-1 h-5 w-5 text-gray-900">
+              <path d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" />
+            </svg>
           </button>
-          <svg className="-mr-1 h-5 w-5 text-gray-900">
-            <path d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" />
-          </svg>
         </div>
         <div className="text-right">
           <button onClick={toggleTheme}>
