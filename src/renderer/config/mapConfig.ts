58ede3521ac7fc3d3089b1d67e45606099976974
Sergio Toro <sergio.toro.castano@gmail.com>
2026-03-06 10:16:34 +0100

    feat: add dotenv for environment variable management
    Integrate dotenv package to manage environment variables, allowing for secure handling of sensitive data. Update MAPTILER_API_KEY to default to an empty string if not set. Update package.json and yarn.lock to include dotenv dependency.

--- src/renderer/config/mapConfig.ts
+++ src/renderer/config/mapConfig.ts
@@ -1,4 +1,3 @@
-export const MAPTILER_API_KEY =
-  process.env.MAPTILER_API_KEY || "4Px7aantyCi3kqWAKq3e";
+export const MAPTILER_API_KEY = process.env.MAPTILER_API_KEY || "";
 
 export const MAPTILER_STYLE_URL = `https://api.maptiler.com/maps/outdoor-v2/style.json?key=${MAPTILER_API_KEY}`;
--- tools/webpack/webpack.plugins.js
+++ tools/webpack/webpack.plugins.js
@@ -1,13 +1,19 @@
-const webpack = require('webpack');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const { inDev } = require('./webpack.helpers');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const { inDev } = require("./webpack.helpers");
+
+dotenv.config();
 
 module.exports = [
   new ForkTsCheckerWebpackPlugin(),
   inDev() && new webpack.HotModuleReplacementPlugin(),
   inDev() && new ReactRefreshWebpackPlugin(),
   new webpack.DefinePlugin({
-    'process.env.MAPTILER_API_KEY': JSON.stringify(process.env.MAPTILER_API_KEY || ''),
+    "process.env.MAPTILER_API_KEY": JSON.stringify(
+      process.env.MAPTILER_API_KEY || ""
+    ),
   }),
 ].filter(Boolean);
